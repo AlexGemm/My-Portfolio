6e88f99303c7161cd0481735a5e1f1cd83d6f7be
AlexG <agemmell@student.fitchburgstate.edu>
2021-10-16 11:15:23 -0400

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿const canvas = document.getElementById('ballCanvas');
+const context = canvas.getContext('2d');
+
+var ball = {
+
+    radius: 100,
+    ballX: canvas.height / 2,
+    ballY: canvas.width / 2
+
+};
+
+function drawBall() {
+
+    ctx.fillStyle = "#ffffff";
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+
+}
+
+drawBall();
