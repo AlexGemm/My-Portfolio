744519f9f4f24704e04a7bcc364897b77fe7bd78
AlexG <agemmell@student.fitchburgstate.edu>
2021-11-24 01:43:35 -0500

    Added a javascript graphical widget

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,10 @@
-﻿
+﻿var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+setInterval(function () {
+    ctx.fillStyle = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}, 1000);
